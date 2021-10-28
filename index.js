63d56756e22832c50c97baea2e7b4095bebb110f
Jobayer Hosen <juba2732@gmail.com>
2021-10-28 19:58:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.judr2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  // ADD USER API
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("vol-net");
+    const usersCollection = db.collection("users");
+
+    app.post("/users/addUser", async (req, res) => {
+      const doc = req.body;
+
+      const result = await usersCollection.insertOne(doc);
+      console.log("inserted user : ", result);
+
+      res.json(doc);
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+//-----------------------------
+
+app.listen(port, () => {
+  console.log("Listening to port: ", port);
+});
